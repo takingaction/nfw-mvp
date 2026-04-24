381d4af865abf7c62bb28304c74fe6c313dc2d43
Ron Passaro <ronpassaro@gmail.com>
2026-04-24 14:55:14 -0400

    Add Meta Pixel (Facebook) tracking with Termly Auto Blocker support

--- app/layout.tsx
+++ app/layout.tsx
@@ -60,9 +60,23 @@ export default async function RootLayout({
         {!isPublicRoute && <Footer />}
         {!isPublicRoute && <BackToTop />}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-MXX079LCCS"
+          src="https://connect.facebook.net/en_US/fbevents.js"
           strategy="afterInteractive"
         />
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1265927739923182');
+            fbq('track', 'PageView');
+          `}
+        </Script>
         <Script id="ga4" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -71,6 +85,15 @@ export default async function RootLayout({
             gtag('config', 'G-MXX079LCCS');
           `}
         </Script>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-MXX079LCCS"
+          strategy="afterInteractive"
+        />
+        <noscript>
+          <img height="1" width="1" style="display:none"
+            src="https://www.facebook.com/tr?id=1265927739923182&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
