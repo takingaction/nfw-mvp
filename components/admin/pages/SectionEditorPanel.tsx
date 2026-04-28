b842020fd51e392b9a662e6a60c5984b4285ed45
Ron Passaro <ronpassaro@gmail.com>
2026-04-28 17:26:22 -0400

    Fix toggle switch - w-11 container, circle properly positioned left when off

--- components/admin/pages/SectionEditorPanel.tsx
+++ components/admin/pages/SectionEditorPanel.tsx
@@ -199,13 +199,13 @@ function FieldEditor({
         <button
           type="button"
           onClick={() => onChange(!value)}
-          className={`relative w-12 h-6 rounded-full transition-colors ${
+          className={`relative w-11 h-6 rounded-full transition-colors ${
             value ? "bg-nfw-wisteria" : "bg-nfw-blackberry/20"
           }`}
         >
           <span
-            className={`absolute top-1 w-4 h-4 bg-white rounded-full transition-transform ${
-              value ? "translate-x-1" : "translate-x-7"
+            className={`absolute top-0.5 left-0.5 w-5 h-5 bg-white rounded-full transition-transform ${
+              value ? "translate-x-5" : "translate-x-0"
             }`}
           />
         </button>
